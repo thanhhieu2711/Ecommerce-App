e9abcd7960281ec2fa2b183545fbd63314542767
thanhhieu2711 <lethanhhieu2711@gmail.com>
2024-04-03 16:02:30 +0700

    Feat:add meta tag Fix: cannot request to api orders

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
     try {
         const _req = await req.json();
 
