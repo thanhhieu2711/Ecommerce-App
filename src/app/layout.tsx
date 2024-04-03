67771f25ac2db5546989998e10a40bccddf40d7b
thanhhieu2711 <lethanhhieu2711@gmail.com>
2024-04-03 16:14:26 +0700

    Fix: metadata rootweb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,10 @@ import RootLayoutCtn from '@/layouts/RootLayout';
 import AppProviders from './app-providers';
 
 export const metadata: Metadata = {
-    title: `Mua Điện Thoại Chính Hãng | Giao Hàng Nhanh | ${process.env.APP_NAME}`,
+    title: `Mua Điện Thoại Chính Hãng | Giao Hàng Nhanh | ${process.env.NEXT_APP_NAME}`,
     description:
         'Khám phá bộ sưu tập điện thoại thông minh hàng đầu từ các thương hiệu hàng đầu tại [Raven Store]. Từ iPhone đến Samsung Galaxy, chúng tôi cung cấp một loạt các điện thoại mới nhất với giá cả phải chăng và chất lượng đảm bảo. Đặt mua ngay hôm nay và nhận ưu đãi độc quyền!',
-    colorScheme: 'dark',
+    colorScheme: 'light',
     viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 };
 
