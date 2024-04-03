9522d9a8c763683d1a5be36c63358c3330cbd60c
thanhhieu2711 <lethanhhieu2711@gmail.com>
2024-04-03 16:09:12 +0700

    Refactor: metadata tag

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -11,19 +11,6 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, maximum-scale=1"
-            />
-            <meta
-                name="title"
-                content="Mua Điện Thoại Chính Hãng | Giao Hàng Nhanh | Raven Store"
-            />
-            <meta
-                name="description"
-                content="Khám phá bộ sưu tập điện thoại thông minh hàng đầu từ các thương hiệu hàng đầu tại [Raven Store]. Từ iPhone đến Samsung Galaxy, chúng tôi cung cấp một loạt các điện thoại mới nhất với giá cả phải chăng và chất lượng đảm bảo. Đặt mua ngay hôm nay và nhận ưu đãi độc quyền!"
-            />
-            <meta property="og:image" content="URL_hình_ảnh" />
             <body className={inter.className}>{children}</body>
         </html>
     );
